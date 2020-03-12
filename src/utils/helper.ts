4efeaa872b724c5c1e5bda4e23690c59c4567474
hristo2612 <hristoapps@gmail.com>
2020-03-13 00:23:40 +0200

    Add new helper function to find A - Function name B - Arguments for givent command

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,19 @@
+import { ICommand } from './../interfaces/commands';
+
 export const parse = (fileContent) => {
   return JSON.parse(fileContent);
+}
+
+export const parseCommandPath = (command: ICommand) => {
+  const functionName = Object.keys(command)[0];
+  let newFunctionName;
+  let args = Object.values(command[functionName]);
+  if (functionName === 'mouse' || functionName === 'keyboard') {
+    newFunctionName = Object.keys(command[functionName])[0];
+    args = Object.values(command[functionName][newFunctionName]);
+  }
+  return {
+    functionName: newFunctionName ? newFunctionName : functionName,
+    args
+  }
 }
