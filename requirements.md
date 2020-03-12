cf71fd56aeba482c014d6330c645464d0c369775
hristo2612 <hristoapps@gmail.com>
2020-03-13 00:23:49 +0200

    Update crawl file

---
+++ requirements.md
@@ -0,0 +1,11 @@
+A. Crawling Foreign Car Website Requirements ( mobile.de )
+  1. Go to https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&scopeId=C&sfmr=false&sortOption.sortBy=creationTime&sortOption.sortOrder=DESCENDING
+  2. Car by car extract valuable information
+  3. Go to point B. with extracted information
+
+B. Crawling Native Car Website Requirements ( mobile.bg )
+
+  1. Go to main page ( mobile.bg )
+  2. Enter crawled foreign car filters
+  3. Find price for foreign car in Bulgaria
+  4. See if it is a great offer
--- src/crawler/crawlWebsite.ts
+++ src/crawler/crawlWebsite.ts
@@ -1,1 +1,30 @@
-console.log('Start crawling', process.argv[2]);
+import { parseCommandPath } from './../utils/helper';
+import { ICommands } from './../interfaces/commands';
+import Nightmare from 'nightmare';
+import { webConfig } from './../config/webConfig';
+import vo from 'vo';
+
+const currentConfig: ICommands = webConfig[process.argv[2]];
+
+vo(run)((err, res) => {
+    if (err) throw err;
+
+    console.log(res);
+});
+
+function *run() {
+    const nightmare = Nightmare({ show: true });
+    let result;
+    const commands = currentConfig.commands;
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < commands.length; i++) {
+        const { functionName, args } = parseCommandPath(commands[i]);
+        result = yield nightmare
+            .useragent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36')
+            [functionName](...args)
+    }
+
+    yield nightmare.end();
+
+    return result;
+}
